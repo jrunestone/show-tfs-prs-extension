f72001782b6ab45cf849cbb6b97968810301f3ff
Johan Runsten <johanrunsten86@gmail.com>
2020-05-29 20:43:46 +0200

    Changed to last commit date

--- all-pull-requests.js
+++ all-pull-requests.js
@@ -21,22 +21,26 @@ const fetchPrs = async () => {
         let prObjs = [];
 
         if (prs.value) {
-            prObjs = prs.value.map(pr => {
-                const createdDate = moment(pr.creationDate);
-                const dateDiffDays = moment().diff(createdDate, 'days');
+            prObjs = await Promise.all(prs.value.map(async pr => {
+                const prCommitsRes = await fetch(`${pr.url}/commits`)
+                const prCommits = await prCommitsRes.json();
+                const lastCommitDate = (prCommits.value && prCommits.value.length) ? moment(prCommits.value[0].author.date) : moment(pr.creationDate);
+
+                const updatedDate = moment(lastCommitDate);
+                const dateDiffDays = moment().diff(updatedDate, 'days');
 
                 return {
                     name: pr.title,
                     author: pr.createdBy.displayName,
-                    dateObj: createdDate,
-                    date: createdDate.format('YYYY-MM-DD HH:MM'),
+                    dateObj: updatedDate,
+                    date: updatedDate.format('YYYY-MM-DD HH:MM'),
                     overdueDays: dateDiffDays,
                     overdueStatus: dateDiffDays > 10 ? 'ancient' : (dateDiffDays > 7 ? 'old' : (dateDiffDays > 4 ? 'overdue' : (dateDiffDays > 2 ? 'due' : 'current'))),
                     url: prUrl.replace('{proj}', proj.name).replace('{repo}', pr.repository.name).replace('{pr}', pr.pullRequestId),
                     source: pr.sourceRefName.replace('refs/heads/', ''),
                     target: pr.targetRefName.replace('refs/heads/', '')
                 };
-            });
+            }));
 
             prObjs.sort((a, b) => b.dateObj.diff(a.dateObj, 'days'));
         }
